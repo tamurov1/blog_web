e51f20c9900c79d92edb1060f1e003fc9dae8bdb
Dmytrii Tamurov <tamurov@sheridancollege.ca>
2025-07-18 19:19:13 -0400

    postlist update

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -19,7 +19,6 @@ export default function PostList() {
             <span>By {post.author || 'Unknown Author'}</span> &nbsp;|&nbsp;{' '}
             <time dateTime={post.date}>{post.date || 'Unknown Date'}</time>
           </p>
-          <p className="text-sm text-gray-500 mt-1">Link: {post.path}</p>
         </a>
       ))}
     </div>
